454d11c7c75530b3bacc80948594d85ea52aa620
Matthew Spofford <matthewspofford@verizon.net>
2018-01-09 17:26:43 -0500

    new commit

--- TestProgram/src/program.java
+++ TestProgram/src/program.java
@@ -3,6 +3,7 @@ public class program {
 	
 	public static void main(String[] args)
 	{
+		mystery1(321);
 		int[] arr = {2, 4, 2, 3, 4, 1, 10, 5};
 		String str = "ababc";
 		int temp = countAbc(str);
@@ -10,6 +11,16 @@ public class program {
 			System.out.print("GOOD");
 		else
 			System.out.print("BAD");
+	}
+	
+	public static void mystery1(int x)
+	{
+		System.out.print(x % 10);
+		if((x / 10) != 0)
+		{
+			mystery1(x / 10);
+		}
+		System.out.print(x % 10);
 	}
 	
 	public static int countAbc(String str) {
@@ -41,19 +52,19 @@ public class program {
 	}
 	
 	public static boolean array220(int[] nums, int index) {
-		  if(nums.length == 0)
-		    return false;
-		    
-		  if(nums.length > index + 1)
-		  {
-		    if(nums[index] == nums[index+1]/10)
-		      return true;
-		    else
-		      return array220(nums, ++index);
-		  }
-		  else
-		    return false;
-		}
+	  if(nums.length == 0)
+	    return false;
+	    
+	  if(nums.length > index + 1)
+	  {
+	    if(nums[index] == nums[index+1]/10)
+	      return true;
+	    else
+	      return array220(nums, ++index);
+	  }
+	  else
+	    return false;
+	}
 
 
 }
