95bfab5aea0f783880e55ecc47f92ba4ac1cce6f
Matthew Spofford <matthewspofford@verizon.net>
2018-01-09 12:53:58 -0500

    new commit

--- TestProgram/src/program.java
+++ TestProgram/src/program.java
@@ -4,12 +4,30 @@ public class program {
 	public static void main(String[] args)
 	{
 		int[] arr = {2, 4, 2, 3, 4, 1, 10, 5};
-		boolean temp = array220(arr,0);
-		if(temp)
+		String str = "ababc";
+		int temp = countAbc(str);
+		if(temp == 2)
 			System.out.print("GOOD");
 		else
 			System.out.print("BAD");
 	}
+	
+	public static int countAbc(String str) {
+	  int posABC = str.indexOf("abc");
+	  int posABA = str.indexOf("aba");
+	  
+	  if(posABC == -1 && posABA == -1)
+	    return 0;
+	  else if(posABC > -1 && posABA == -1)
+	    return 1 + countAbc(str.substring(1 + posABC));
+	  else if(posABA > -1 && posABC == -1)
+	    return 1 + countAbc(str.substring(1 + posABA));
+	  else if(posABC < posABA)
+	    return 1 + countAbc(str.substring(1 + posABC));
+	  else
+	    return 1 + countAbc(str.substring(1 + posABA));
+	}
+
 	
 	public static int array11(int[] nums, int index) {
 	  if(nums.length == 0)
