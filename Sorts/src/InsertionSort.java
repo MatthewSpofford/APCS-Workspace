2ae69e9a1b4d6b17a7a7558efad518ada83814d2
Matthew Spofford <matthewspofford@verizon.net>
2018-02-06 13:55:47 -0500

    new commit

--- Sorts/src/InsertionSort.java
+++ Sorts/src/InsertionSort.java
@@ -11,11 +11,13 @@ public class InsertionSort {
 				if(array[j] < array[minIndex])
 					minIndex = j;
 			}
-			
+			System.out.print("1.  ");
+			print(array);
 			for(int k = i; k > minIndex; k--)
 			{
 				array[k] = array[k - 1];
 			}
+			System.out.print("2.  ");
 			array[minIndex] = array[i];
 			print(array);
 		}
