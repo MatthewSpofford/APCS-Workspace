cc7552e5256e41d77eb0984770fea84741cb649e
Matthew Spofford <matthewspofford@verizon.net>
2018-02-08 16:28:16 -0500

    new commit

--- Sorts/src/MergeSort.java
+++ Sorts/src/MergeSort.java
@@ -1,18 +1,65 @@
+import java.util.Arrays;
 
 public class MergeSort implements Sorts{
 
 	public void sort(int[] array)
 	{
-		doMergeSort(array);
+		doMergeSort(array, 0, array.length);
 	}
 	
-	private void doMergeSort(int[] array)
+	private void doMergeSort(int[] array, int start, int end)
 	{
+		if(end - start <= 1)
+		{
+			return;
+		}
+		else
+		{
+			int aStart = start;
+			int aEnd = (end + start)/2;
+			int bStart = (end + start)/2;
+			int bEnd = end;
+			doMergeSort(array, aStart, aEnd);
+			doMergeSort(array, bStart, bEnd);
+			merge(array, aStart, aEnd, bStart, bEnd);
+			return;
+		}
+	}
+	
+	private void merge(int[] array, int aStart, int aEnd, int bStart, int bEnd)
+	{
+		int[] output = new int[(aEnd - aStart) + (bEnd - bStart)];
+		int aIndex = aStart;
+		int bIndex = bStart;
+		int index = 0;
+		while(index < output.length)
+		{
+			if(aIndex >= aEnd)
+			{
+				for(int i = bIndex; i < bEnd; i++)
+					output[index++] = array[i];
+			}
+			else if(bIndex >= bEnd)
+			{
+				for(int i = aIndex; i < aEnd; i++)
+					output[index++] = array[i];
+			}
+			else
+			{
+				if(array[aIndex] < array[bIndex])
+				{
+					output[index++] = array[aIndex++];
+				}
+				else
+				{
+					output[index++] = array[bIndex++];
+				}
+			}
+		}
 		
-	}
-	
-	private int[] merge(int[] a, int[] b)
-	{
-		return new int[0];
+		for(int i = 0; i < output.length; i++)
+		{
+			array[i + aStart] = output[i];
+		}
 	}
 }
--- Sorts/src/QuickSort.java
+++ Sorts/src/QuickSort.java
@@ -3,6 +3,11 @@ public class QuickSort implements Sorts{
 	
 	public void sort(int[] array)
 	{
+		doQuickSort(array, array.length/2);
+	}
+	
+	private void doQuickSort(int[] array, int pivot)
+	{
 		
 	}
 }
