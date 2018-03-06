eaede1aeccdd1906f9d5b1b32db9b430354ce39a
Matthew Spofford <matthewspofford@verizon.net>
2018-03-06 12:36:26 -0500

    new commit

---
+++ Elevens Lab/src/CardTester.java
@@ -0,0 +1,20 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card1 = new Card("King", "Hearts", 10);
+		Card card2 = new Card("5", "Clubs", 5);
+		Card card3 = new Card("King", "Hearts", 10);
+		
+		System.out.println(card1 + "\n" + card2 + "\n" +card3);
+		System.out.println(card1.matches(card3));
+		System.out.println(card3.matches(card1));
+		System.out.println(card1.matches(card2));
+	}
+}
