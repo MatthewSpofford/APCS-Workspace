203dddbba6f893bac7dec262be9aec625c3a175c
Matthew Spofford <matthewspofford@verizon.net>
2018-03-27 11:49:38 -0400

    fdsafdsa

--- Elevens Lab/src/CrazyThirteensBoard.java
+++ Elevens Lab/src/CrazyThirteensBoard.java
@@ -9,14 +9,13 @@ public class CrazyThirteensBoard extends ThirteensBoard {
 	public CrazyThirteensBoard() {
 		super();
 	}
-	
 
 	/**
 	 * Determine if there are any legal plays left on the board.
 	 * In Thirteens, the legal groups are: 
-	 * <p>(1) a pair of non-face cards whose values add to 13</p> 
+	 * <p>(1) a pair of non-face cards whose values add to 13.
+	 * If the pair selected contains a face card, put the other card back into the deck.</p> 
 	 * <p>(2) a single card consisting of a king.</p>
-	 * <p>(3)</p>
 	 * @return true if there is a legal play left on the board;
 	 *         false otherwise.
 	 */
@@ -28,9 +27,9 @@ public class CrazyThirteensBoard extends ThirteensBoard {
 	/**
 	 * Determines if the selected cards form a valid group for removal.
 	 * In Thirteens, the legal groups are: 
-	 * <p>(1) a pair of non-face cards whose values add to 13</p> 
+	 * <p>(1) a pair of non-face cards whose values add to 13.
+	 * If the pair selected contains a face card, put the other card back into the deck.</p> 	 
 	 * <p>(2) a single card consisting of a king.</p>
-	 * <p>(3)</p>
 	 * @param selectedCards the list of the indices of the selected cards.
 	 * @return true if the selected cards form a valid group for removal;
 	 *         false otherwise.
