89bdd93dcc734718654f69f84185566f14d16292
Matthew Spofford <matthewspofford@verizon.net>
2018-04-13 08:37:50 -0400

    Prep stuff for AP Exam

--- random stuff/src/Test.java
+++ random stuff/src/Test.java
@@ -2,8 +2,14 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int[] array = {4, 1, 10, 3, 6, 3, 6, 10, 9, 12, 5};
-		doSome(array, 5);
+//		int[] array = {4, 1, 10, 3, 6, 3, 6, 10, 9, 12, 5};
+//		doSome(array, 5);
+		
+		mysteryMix("la-la-la!");
+		//la-la-la!
+		//la-	la-		la!
+		//l -			l !
+		//		ala-a
 	}
 	
 	public static void doSome(int[] arr, int lim) {
@@ -13,11 +19,20 @@ public class Test {
 		while (k < arr.length && arr[k] < lim) {
 			if(arr[k] > v) {
 				v = arr[k];
-				t++;
+				t++;	//Statement T
 			}
-			k++;
+			k++;	//Statement S
 		}
 		
 		System.out.println("V: " + v + "\nT: " + t + "\nS: " + k);
+	}
+	
+	public static void mysteryMix(String str) {
+		int len = str.length();
+		if(len >= 3) {
+			mysteryMix(str.substring(0, len/3));
+			System.out.print(str.substring(len/3, 2*len/3));
+			mysteryMix(str.substring(2*len/3));
+		}
 	}
 }
